ec32c4b411efe3874a177831b20308c3fbba7488
Илья Лавров <lavrov.ilya2277@mail.ru>
2026-04-16 14:55:15 +0300

    Update revenue_distribution_by_event_type.sql

--- Project_Yandex.Afisha/SQL/revenue_distribution_by_event_type.sql
+++ Project_Yandex.Afisha/SQL/revenue_distribution_by_event_type.sql
@@ -1,3 +1,14 @@
+-- Для заказов в рублях вычислите распределение количества заказов и их выручку в зависимости от типа мероприятия event_type_main. Результат должен включать поля:
+-- тип мероприятия event_type_main;
+-- общая выручка с заказов total_revenue;
+-- количество заказов total_orders;
+-- средняя стоимость заказа avg_revenue_per_order;
+-- уникальное число событий total_event_name (по их коду event_name_code);
+-- среднее число билетов в заказе avg_tickets;
+-- средняя выручка с одного билета avg_ticket_revenue;
+-- доля выручки от общего значения revenue_share, округлённая до трёх знаков после точки.
+-- Результат отсортируйте по убыванию значения в поле total_orders.
+
 SELECT event_type_main, 
        SUM(revenue) AS total_revenue,
        COUNT(order_id) AS total_orders,
