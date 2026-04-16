579f353d2c5e4d817b7753f6f619ffa43961f3ac
Илья Лавров <lavrov.ilya2277@mail.ru>
2026-04-16 14:52:51 +0300

    Update general_values_of_key_indicators.sql

--- Project_Yandex.Afisha/SQL/general_values_of_key_indicators.sql
+++ Project_Yandex.Afisha/SQL/general_values_of_key_indicators.sql
@@ -1,3 +1,9 @@
+-- Задача: Вычислите общие значения ключевых показателей сервиса за весь период:
+-- общая выручка с заказов total_revenue;
+-- количество заказов total_orders;
+-- средняя выручка заказа avg_revenue_per_order;
+-- общее число уникальных клиентов total_users.
+-- Значения посчитайте в разрезе каждой валюты (поле currency_code). Результат отсортируйте по убыванию значения в поле total_revenue.
 SELECT currency_code, 
        SUM(revenue) AS total_revenue,
        COUNT(order_id) AS total_orders,
