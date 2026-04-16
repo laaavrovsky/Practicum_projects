2537f8d42f53b19c3ad0586ed08e504c54f8a504
Илья Лавров <lavrov.ilya2277@mail.ru>
2026-04-16 14:51:19 +0300

    Update dynamics_of_value_changes.sql

--- Project_Yandex.Afisha/SQL/dynamics_of_value_changes.sql
+++ Project_Yandex.Afisha/SQL/dynamics_of_value_changes.sql
@@ -1,3 +1,4 @@
+-- Задача: Для заказов в рублях вычислите изменение выручки, количества заказов, уникальных клиентов и средней стоимости одного заказа в недельной динамике. Результат отсортируйте по возрастанию значения в поле week.
 SELECT DATE_TRUNC('week', created_dt_msk)::date AS week, 
        SUM(revenue) AS total_revenue, 
        COUNT(order_id) AS total_orders,
