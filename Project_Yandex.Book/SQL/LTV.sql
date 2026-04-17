607539654210367339e011167a2432f5325d8c74
Илья Лавров <lavrov.ilya2277@mail.ru>
2026-04-17 17:31:01 +0300

    Update LTV.sql

--- Project_Yandex.Book/SQL/LTV.sql
+++ Project_Yandex.Book/SQL/LTV.sql
@@ -5,28 +5,22 @@
 -- ltv — средний LTV пользователей в городе или регионе.Округлите до двух знаков после запятой.
 
 WITH user_months AS (
-    SELECT DISTINCT
-        ba.puid,
-        bg.usage_geo_id_name AS city,
-        DATE_TRUNC('month', ba.msk_business_dt_str) AS month
+    SELECT DISTINCT ba.puid,
+                    bg.usage_geo_id_name AS city,
+                    DATE_TRUNC('month', ba.msk_business_dt_str) AS month
     FROM bookmate.audition ba
-    JOIN bookmate.geo bg
-        ON ba.usage_geo_id = bg.usage_geo_id
+    JOIN bookmate.geo bg ON ba.usage_geo_id = bg.usage_geo_id
     WHERE bg.usage_geo_id_name IN ('Москва', 'Санкт-Петербург')
 ),
-
 user_revenue AS (
-    SELECT
-        puid,
-        city,
-        COUNT(month) * 399 AS revenue
+    SELECT puid,
+           city,
+           COUNT(month) * 399 AS revenue
     FROM user_months
     GROUP BY puid, city
 )
-
-SELECT
-    city,
-    COUNT(puid) AS total_users,
-    ROUND(SUM(revenue) * 1.0 / COUNT(puid), 2) AS ltv
+SELECT city,
+       COUNT(puid) AS total_users,
+       ROUND(SUM(revenue) * 1.0 / COUNT(puid), 2) AS ltv
 FROM user_revenue
 GROUP BY city;
